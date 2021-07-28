d0f04df30e393fc0ff41a666570f2644e3016cce
QPThree <youngqp3@gmail.com>
2021-07-28 08:48:05 -0700

    basic form and state are done

---
+++ src/City.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, Button} from 'react-bootstrap';
+import axios from 'axios';
+
+class City extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        cityToSearch: '',
+        cityData: {},
+    }
+  }
+  getLocation = async (e) =>{
+  //  function will use city stored in state to search api with axios
+    e.preventDefault();
+    console.log('inside getLocation fx');
+    console.log(this.state.cityToSearch);
+    let locationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env}&q=${this.state.cityToSearch}&format=json`);
+    this.setState({
+      cityData: locationData,
+    })
+    console.log(this.state.cityToSearch);
+  }
+  handleCityInput = (e) =>{
+    e.preventDefault();
+    this.setState({
+      cityToSearch: e.target.value,
+    })
+  };
+  
+  render() {
+    return (
+      <Form>
+
+      <Form.Group>
+        <Form.Control onChange={this.handleCityInput} type="text" placeholder="Enter City" />
+      <Button variant="primary" type="submit"  onClick = {this.getLocation}>Explore City!</Button>
+      </Form.Group>
+      </Form>
+    )
+  }
+}
+
+export default City;
