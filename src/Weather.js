105387e06b8eb9b6af59dbaff98f90009ebff9d4
QPThree <youngqp3@gmail.com>
2021-08-01 12:47:30 -0700

    added images to weather accordion pane

--- src/Weather.js
+++ src/Weather.js
@@ -1,34 +1,63 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table';
-
-
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import weatherImage1 from './assets/img/weather.jpg'
+import weatherImage2 from './assets/img/weather2.jpg'
+import weatherImage3 from './assets/img/weather3.jpg'
 class Weather extends React.Component {
-  displayData = (obj) => {  
-      return (<Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th className = 'weatherHeader'>{new Date(obj.threeDayDates[0]).toDateString()}</th>
-            <th className = 'weatherHeader'>{new Date(obj.threeDayDates[1]).toDateString()}</th>
-            <th className = 'weatherHeader'>{new Date(obj.threeDayDates[2]).toDateString()}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className = 'weatherData'>{obj.threeDayDescription[0]}</td>
-            <td className = 'weatherData'>{obj.threeDayDescription[1]}</td>
-            <td className = 'weatherData'>{obj.threeDayDescription[2]}</td>
-          </tr>
-        </tbody>
-      </Table>)
+  displayData = (obj) => {
+    return (
+      
+        <CardGroup className='shadow-sm p-3 mb-5 bg-white rounded'>
+          <Card className='weatherCard '>
+            
+            <Card.Body>
+              <Card.Title className='weatherHeader'>{new Date(obj.threeDayDates[0]).toDateString()}</Card.Title>
+              <Card.Text className='weatherData'>
+                {obj.threeDayDescription[0]}
+              </Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src={weatherImage1} height='60%'/>
+            <Card.Footer>
+              <small className="text-muted"></small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            
+            <Card.Body>
+              <Card.Title className='weatherHeader'>{new Date(obj.threeDayDates[1]).toDateString()}</Card.Title>
+              <Card.Text className='weatherData'>
+                {obj.threeDayDescription[1]}
+              </Card.Text>
+            </Card.Body>
+            <Card.Img variant="top" src={weatherImage2} height='60%' />
+            <Card.Footer>
+
+            </Card.Footer>
+          </Card>
+          <Card>
+            
+            <Card.Body>
+              <Card.Title className='weatherHeader'>{new Date(obj.threeDayDates[2]).toDateString()}</Card.Title>
+              <Card.Text className='weatherData'>
+                {obj.threeDayDescription[2]}
+              </Card.Text>
+            </Card.Body>
+            <Card.Img variant="top" src={weatherImage3} height='60%'/>
+            <Card.Footer>
+
+            </Card.Footer>
+          </Card>
+        </CardGroup>)
   }
 
-  render() {
+        render() {
 
     return (
-      <h2>{this.displayData(this.props.data)}</h2>
-    )
+        <h2>{this.displayData(this.props.data)}</h2>
+        )
   }
 }
 
-export default Weather;
+        export default Weather;
 //this.props.data.data[0].threeDayDates[0]
